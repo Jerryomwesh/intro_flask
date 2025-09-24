1a22ba42e20cff794db8ecf54b1b47b653c5cfad
Jerryomwesh <jerryomwesh@gmail.com>
2025-09-24 13:00:45 +0300

    updated students list

--- app/routes/student.py
+++ app/routes/student.py
@@ -1,4 +1,6 @@
 from flask import Blueprint,jsonify,request
+from ..models import Student 
+from ..db import db
 
 #create student bluprint
 student_bp=Blueprint("student",__name__,url_prefix="/student")
@@ -15,9 +17,37 @@ def single_student():
 def add_user_json():
     print("Add user was hit")
     data=request.get_json() #Body to json as python dic
-    print("Received Data",data)
-    # Read the request
-    return "Adding a student",200
+
+    name=data.get("name")
+    email=data.get("email")
+
+    if not name or not email:
+        return jsonify ({"error":"Name and email are required"}),400
+    
+    #check for existing email
+    exists=Student.query.filter_by(email=email).first()
+    if exists:
+        return jsonify({"error":"Email already exists"}),400
+    
+    new_student=Student(name=name,email=email)
+    # new_student.save() #save to db
+    db.session.add(new_student)
+    db.session.commit()
+
+    return jsonify({
+        "message":"Student added successfully",
+        "student":{
+            "id":new_student.id,
+            "name":new_student.name,
+            "email":new_student.email,
+            "created_at":new_student.created_at.isoformat()
+            }
+        }),201
+
+
+    # print("Received Data",data)
+    # # Read the request
+    # return "Adding a student",200
 
 @student_bp.route("/add/form",methods=["POST"])
 def add_user_form():
@@ -31,5 +61,23 @@ def edit_student():
 
 @student_bp.route("/list",methods=["GET"])
 def list_users():
-    print("List Students")
-    return "List All students"
+    # print("List Students")
+    # return "List All students"
+
+    students=Student.query.all()
+
+    print(students)
+
+    student_list=[]
+
+    for student in students:
+        student_list.append({
+            "id":student.id,
+            "name":student.name,
+            "email":student.email,
+            "created_at":student.created_at.isoformat()
+        })
+
+    return jsonify(student_list),200
+
+
